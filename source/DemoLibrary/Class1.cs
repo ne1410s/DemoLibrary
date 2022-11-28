0a3cd6e1c72f1995a695740f27cd79e76bff6bc4
paul.jones <paul.jones@animalfriends.co.uk>
2022-11-28 11:45:40 +0000

    latest changes

--- source/DemoLibrary/Class1.cs
+++ source/DemoLibrary/Class1.cs
@@ -1,16 +1,25 @@
-﻿namespace DemoLibrary
+﻿// <copyright file="Class1.cs" company="ne1410s">
+// Copyright (c) ne1410s. All rights reserved.
+// </copyright>
+
+namespace DemoLibrary;
+
+/// <summary>
+/// A class.
+/// </summary>
+public class Class1
 {
     /// <summary>
-    /// A class.
+    /// Initializes a new instance of the <see cref="Class1"/> class.
     /// </summary>
-    public class Class1
-    {
-        /// <summary>
-        /// Adds two numbers.
-        /// </summary>
-        /// <param name="n1">The first number.</param>
-        /// <param name="n2">The second number.</param>
-        /// <returns>The sum.</returns>
-        public int Add(int n1, int n2) => n1 + n2;
-    }
+    public Class1()
+    { }
+
+    /// <summary>
+    /// Adds two numbers.
+    /// </summary>
+    /// <param name="n1">The first number.</param>
+    /// <param name="n2">The second number.</param>
+    /// <returns>The sum.</returns>
+    public int Add(int n1, int n2) => n1 + n2;
 }
